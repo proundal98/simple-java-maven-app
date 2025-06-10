5996726ffb5cfda1316c017bd4bf3a626aab84dc
proundal98 <roundalpp1998@gmail.com>
2025-06-10 11:43:57 +0530

    fix.bugs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,9 @@
-/*package com.mycompany.app;
+package com.mycompany.app;
 
 /**
  * Hello world!
  */
-//*public class App {
+public class App {
 
     private static final String MESSAGE = "Hello World!";
 
